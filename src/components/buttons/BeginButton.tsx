c26bd68b1f9a9d29d060c8466ac2cb92041c43dc
todddenmar <todddenmar@gmail.com>
2024-09-25 18:57:16 +0800

    checking dark mode system

--- src/components/buttons/BeginButton.tsx
+++ src/components/buttons/BeginButton.tsx
@@ -4,7 +4,7 @@ import AIIcon from '../icons/AIIcon';
 function BeginButton() {
   return (
     <div className="relative">
-      <button className="primary-button-bg begin_button h-[56px] w-fit px-[44px] py-4 rounded-full font-spacegrotesk text-[18px] leading-[24px] flex items-center gap-2 relative z-20">
+      <button className="primary-button-bg text-white begin_button h-[56px] w-fit px-[44px] py-4 rounded-full font-spacegrotesk text-[18px] leading-[24px] flex items-center gap-2 relative z-20">
         Begin <AIIcon />
       </button>
     </div>
